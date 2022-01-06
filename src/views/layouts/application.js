36107717356e638cb7e4b4d60d5988ee76c16c49
jlcampbell1991 <joshua.campbell@banno.com>
2022-01-06 08:36:32 -0600

    Add view port to application layout meta

--- src/views/layouts/application.js
+++ src/views/layouts/application.js
@@ -6,6 +6,7 @@ module.exports.application = (title, html, url) => {
     <html lang="en">
     <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>New App - ${title}</title>
         <link rel="stylesheet" href="/stylesheets/style.css" type="text/css" />
     </head>
